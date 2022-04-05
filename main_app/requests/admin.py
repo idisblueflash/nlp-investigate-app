c9fabc18a52c439cf3d31d819031e4f05ecdac78
Flash Hu <idisblueflash@gmail.com>
2022-04-05 20:00:31 +0800

    add question into admin

--- main_app/requests/admin.py
+++ main_app/requests/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Request
+from .models import Request, Question
 
 admin.site.register(Request)
+admin.site.register(Question)
--- main_app/requests/models.py
+++ main_app/requests/models.py
@@ -14,3 +14,6 @@ class Request(models.Model):
 class Question(models.Model):
     request = models.ForeignKey(Request, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.question_text}'
