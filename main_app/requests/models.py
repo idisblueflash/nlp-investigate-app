137eab98e1bf5e01450d898b4918f124a068bb26
Flash Hu <idisblueflash@gmail.com>
2022-04-05 19:55:08 +0800

    initial of app

---
+++ main_app/requests/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class Request(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    pub_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Question(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=256)
